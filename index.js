c340dba20e2db2c32434ae5de9ef6392babc4cb3
Dylan Hays <twentygx@outlook.com>
2022-07-18 18:41:09 -0500

    commit tree 41a4aca4f199e9f5f7a3974d4fdee3c9a53db951

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+const num1 = 31;
+const num2 = 2;
+let multiply = num1 * num2;
+let random = Math.ceil(Math.random() * 10);
+let num3 = 34;
+let num4 = 5;
+let mod = num3 % num4;
+let max = Math.max(1,2,4,8,16,20);
